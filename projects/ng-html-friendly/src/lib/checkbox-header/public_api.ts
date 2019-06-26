81c3f2d7994b2fd8ef53ecc41103b2a499d28e2f
MightLin <might0215@gmail.com>
2019-06-26 11:04:06 +0800

    A

--- projects/ng-html-friendly/src/lib/checkbox-header/public_api.ts
+++ projects/ng-html-friendly/src/lib/checkbox-header/public_api.ts
@@ -1,6 +1,7 @@
 /*
  * Public API Surface of ng-html-friendly
  */
+export * from './checkbox-header-container.directive';
 export * from './checkbox-header.directive';
 export * from './checkbox-header.module';
 
--- projects/ng-html-friendly/src/public_api.ts
+++ projects/ng-html-friendly/src/public_api.ts
@@ -9,6 +9,7 @@ export * from './lib/bin-checkbox/bin-checkbox.module';
 export * from './lib/checked-list/checked-list.directive';
 export * from './lib/checked-list/checked-list.module';
 
+export * from './lib/checkbox-header/checkbox-header-container.directive';
 export * from './lib/checkbox-header/checkbox-header.directive';
 export * from './lib/checkbox-header/checkbox-header.module';
 
